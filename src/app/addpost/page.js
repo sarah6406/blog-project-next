5c6e3531af2befca5e2edd842fec540bcea76600
Sarah Wiseman <s.wiseman777@gmail.com>
2024-05-13 15:23:25 +0100

    fixing bug

--- src/app/addpost/page.js
+++ src/app/addpost/page.js
@@ -7,9 +7,10 @@ export default function AddPost() {
     "use server";
 
     const username = formData.get("username");
+    const title = formData.get("title");
     const message = formData.get("message");
 
-    await sql`INSERT INTO posts (username, message) VALUES (${username}, ${message})`;
+    await sql`INSERT INTO posts (username, title, message) VALUES (${username}, ${title}, ${message})`;
 
     revalidatePath("/posts");
     redirect("/posts");
@@ -18,9 +19,11 @@ export default function AddPost() {
     <div>
       {/* <h1>This is the Add Post route.</h1> */}
       <h2>Add Post!</h2>
-      <form action={handleAddPost}  className="form">
+      <form action={handleAddPost} className="form">
         <label htmlFor="username">Username:</label>
         <input name="username" id="username" placeholder="Username" />
+        <label htmlFor="title">Title:</label>
+        <input name="title" id="title" placeholder="Title" />
         <label htmlFor="message">Message:</label>
         <textarea name="message" id="message" placeholder="Message" />
 
